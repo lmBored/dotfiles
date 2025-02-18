3ff8241674475288a1fea1ef0b18f65087226bd8
I thought this was Instagram <79646021+lmBored@users.noreply.github.com>
2025-02-18 22:14:45 +0100

    Add files via upload

---
+++ template/v0.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+// #define ll long long
+// #define ar array
+// #define endl "\n"
+// #define all(a) a.begin(),a.end()
+// #define fastio ios::sync_with_stdio(false); cin.tie(0);
+// #define f(i,x,n) for(ll i=x;i<n;i++)
+using namespace std;
+
+int32_t main(void)
+{
+    ios::sync_with_stdio(0);
+    cout.tie(0); cin.tie(0);
+
+    return 0;
+}
+
